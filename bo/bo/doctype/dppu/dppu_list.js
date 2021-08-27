f047c7ea9810243990c72d28128e16d716f8a9a1
Perkasa Job <perkasajob@gmail.com>
2021-08-27 05:20:36 +0000

    hide download XLS for DM

--- bo/bo/doctype/dppu/dppu_list.js
+++ bo/bo/doctype/dppu/dppu_list.js
@@ -14,22 +14,24 @@ frappe.listview_settings['DPPU'] = {
 		}
 	},
 	refresh: function(listview) {
-		listview.page.add_menu_item(__('Download XLS'), function(e,o){
-			var method =
-					'/api/method/bo.bo.doctype.dpl.dpl.download_template';
-			var names = listview.get_checked_items().map((o,i)=>{
-				return o.name
-			})
-			if (names.length > 0){
-				var export_filters = { "name": ["in", names]}
-				open_url_post(method, {
-					doctype: listview.doctype,
-					file_type: "Excel",
-					export_fields: {[listview.doctype]:["name","workflow_state","blanko_nr","dm_user","territory","mr_user", "dx_user", "saldo", "cash_transfer","number","date","comment","jml_ccln"]},
-					export_filters: export_filters,
-					export_protect_area: [2, 10, 11],
-				});
-			}
-		});
+		if(!frappe.user.has_role("DM")){
+			listview.page.add_menu_item(__('Download XLS'), function(e,o){
+				var method =
+						'/api/method/bo.bo.doctype.dpl.dpl.download_template';
+				var names = listview.get_checked_items().map((o,i)=>{
+					return o.name
+				})
+				if (names.length > 0){
+					var export_filters = { "name": ["in", names]}
+					open_url_post(method, {
+						doctype: listview.doctype,
+						file_type: "Excel",
+						export_fields: {[listview.doctype]:["name","workflow_state","blanko_nr","dm_user","territory","mr_user", "dx_user", "saldo", "cash_transfer","number","date","comment","jml_ccln"]},
+						export_filters: export_filters,
+						export_protect_area: [2, 10, 11],
+					});
+				}
+			});
+		}
 	}
 };
